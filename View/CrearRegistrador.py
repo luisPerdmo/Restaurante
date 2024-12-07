172fd698e47049651e7aaa6187a6519d26fe25d8
Pedro Perdomo <pedroperdomo@ejemplo.com>
2024-12-07 01:53:21 -0500

    Agregue nuevas funcion y termine todo lo de CrearRegistrador

--- View/CrearRegistrador.py
+++ View/CrearRegistrador.py
@@ -5,7 +5,29 @@ from Tooltip import Tooltip
 
 class CrearRegistrador():
 
+    def limpiarCampos(self, event=None):
+        self.txtNombres.delete(0, tk.END)
+        self.txtApellido.delete(0, tk.END)
+        self.txtEmail.delete(0, tk.END)
+        self.txtCedula.delete(0, tk.END)
+        self.txtPassword.delete(0, tk.END)
+        self.txtNombres.config(bg="#ffffff")
+        self.txtApellido.config(bg="#ffffff")
+        self.txtEmail.config(bg="#ffffff")
+        self.txtCedula.config(bg="#ffffff")
+        self.txtPassword.config(bg="#ffffff")
+
     def guardarRegistrador(self, event):
+        if not self.txtNombres.get() or not self.txtApellido.get() or not self.txtEmail.get() or not self.txtCedula.get() or not self.txtPassword.get():
+            messagebox.showerror("Error", "Por favor ingrese todos los valores en los campos obligatorios.")
+            return 
+        for campo, nombreCampo in [(self.txtNombres.get(), "Nombres"), (self.txtApellido.get(), "Apellido")]:
+            if not campo.replace(" ", "").isalpha():
+                messagebox.showerror("Error", f"El campo '{nombreCampo}' solo puede contener letras.")
+                return
+        if not self.txtCedula.get().isdigit():
+            messagebox.showerror("Error", "El campo 'Cedula' solo puede contener números.")
+            return
         rol = "Registrador"
         if self.Usuario.existeUsuario(self.txtCedula.get()):  
             messagebox.showerror("Error", f"La cédula '{self.txtCedula.get()}' ya está registrada.")
@@ -19,12 +41,37 @@ class CrearRegistrador():
     def mostrarAyuda(self, event):
             mensaje = (
                 "Formulario de registro de usuarios:\n\n"
-                "- Complete todos los campos obligatorios marcados con *.\n"
-                "- Pulse 'Registrar' para guardar los datos.\n"
-                "- Pulse 'Limpiar' para borrar los campos.\n"
-                "- Pulse 'Salir' para cerrar esta ventana."
+                "1. Complete todos los campos obligatorios marcados con *.\n"
+                "2. Pulse 'Registrar' para guardar los datos ingresados.\n"
+                "3. Pulse 'Limpiar' para borrar los datos ingresados en los campos.\n"
+                "4. Pulse 'Salir' para cerrar la ventana de registro.\n\n"
+                "Notas adicionales:\n"
+                "- Los campos de 'Nombres' y 'Apellido' solo aceptan letras.\n"
+                "- El campo 'Cedula' solo acepta números.\n"
+                "- El campo 'Email' debe tener un formato válido.\n"
+                "- La contraseña debe ser segura y contener al menos 8 caracteres."
             )
             messagebox.showinfo("Ayuda", mensaje)
+
+    def soloNumeros(self, event):
+        numeros = event.keysym
+        if numeros.isalpha(): 
+            self.txtCedula.config(bg="#F8D7DA", fg="#000000")
+        else:
+            self.txtCedula.config(bg="#ffffff", fg="#000000")
+        
+    def soloLetras(self, event):
+        letras = event.keysym
+        if event.widget == self.txtNombres:
+            if letras.isdigit() or letras == "BackSpace":
+                self.txtNombres.config(bg="#F8D7DA", fg="#000000")
+            else:
+                self.txtNombres.config(bg="#ffffff", fg="#000000")
+        elif event.widget == self.txtApellido:
+            if letras.isdigit() or letras == "BackSpace":
+                self.txtApellido.config(bg="#F8D7DA", fg="#000000")
+            else:
+                self.txtApellido.config(bg="#ffffff", fg="#000000")
 
     def __init__(self, Usuario):
         self.ventana = tk.Toplevel()
@@ -63,19 +110,22 @@ class CrearRegistrador():
         #Campos de textos
         self.txtNombres = tk.Entry(self.ventana)
         self.txtNombres.place(relx=0.50, rely=0.24, anchor="center")
-        Tooltip(self.txtNombres, "Ingrese su nombre .")
+        self.txtNombres.bind("<KeyPress>", self.soloLetras)
+        Tooltip(self.txtNombres, "Ingrese su nombre. Solo se permiten letras.")
 
         self.txtApellido = tk.Entry(self.ventana)
         self.txtApellido.place(relx=0.50, rely=0.37, anchor="center")
-        Tooltip(self.txtApellido, "Ingrese su Apellido.")
+        self.txtApellido.bind("<KeyPress>", self.soloLetras)
+        Tooltip(self.txtApellido, "Ingrese su apellido. Solo se permiten letras.")
 
         self.txtEmail = tk.Entry(self.ventana)
         self.txtEmail.place(relx=0.50, rely=0.50, anchor="center")
-        Tooltip(self.txtEmail, "Ingrese un correo electrónico válido.")
+        Tooltip(self.txtEmail, "Ingrese un correo electrónico válido en formato usuario@dominio.com.")
 
         self.txtCedula = tk.Entry(self.ventana)
         self.txtCedula.place(relx=0.50, rely=0.63, anchor="center")
-        Tooltip(self.txtCedula, "Ingrese su cedula.")
+        self.txtCedula.bind("<KeyPress>", self.soloNumeros)
+        Tooltip(self.txtCedula, "Ingrese su cédula. Solo se permiten números.")
 
         self.txtPassword = tk.Entry(self.ventana)
         self.txtPassword.place(relx=0.50, rely=0.76, anchor="center")
@@ -89,6 +139,7 @@ class CrearRegistrador():
 
         self.btnLimpiar = tk.Button(self.ventana, image=self.iconoLimpiar, text="Limpiar", width=85, compound="left")
         self.btnLimpiar.place(relx=0.65, rely=0.87, anchor="center")
+        self.btnLimpiar.bind("<Button-1>", self.limpiarCampos)
         Tooltip(self.btnLimpiar, "Limpiar los campos del formulario")
 
         self.btnSalir = tk.Button(self.ventana, image=self.iconoSalir, text="Salir", width=185, compound="left")
