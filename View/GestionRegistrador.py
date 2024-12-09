2ccf8935c368364d5e48aea7abaa80389f09ace0
Pedro Perdomo <pedroperdomo@ejemplo.com>
2024-12-09 13:17:39 -0500

    Termine GestionarRegistro

--- View/GestionRegistrador.py
+++ View/GestionRegistrador.py
@@ -30,6 +30,7 @@ class GestionRegistrador():
             self.btnGestionMesero.place_forget()
             self.btnGestionMesa.place_forget()  
             self.btnGestionComanda.place_forget()
+            self.btnSalir.place_forget()
             self.lblMenu.place_forget()
         else:
             self.barra.configure(width=140)
@@ -38,6 +39,7 @@ class GestionRegistrador():
             self.btnGestionMesero.place(relx=0.01, rely=0.3, anchor="w")
             self.btnGestionMesa.place(relx=0.01, rely=0.4, anchor="w")
             self.btnGestionComanda.place(relx=0.01, rely=0.5, anchor="w")
+            self.btnSalir.place(relx=0.01, rely=0.6, anchor="w")
             self.btnBarra.place(relx=0.89, rely=0.05, anchor="center")
             self.lblMenu = tk.Label(self.barra, text="Menu", bg="#CCD1D1", font=("Times", 20, "bold"))
             self.lblMenu.place(relx=0.43, rely=0.05, anchor="center")
@@ -72,6 +74,7 @@ class GestionRegistrador():
     def eliminarMesa(self):
         EliminarMesa(self.usuario)
 
+    #Mesero
     def crearMenuMesero(self, event):
         if self.barraExpandida:
             self.menuMesero = tk.Menu(self.ventana)
@@ -86,6 +89,7 @@ class GestionRegistrador():
     def eliminarMesero(self): 
         EliminarMesero(self.usuario)
 
+    #Comanda
     def crearMenuComada(self, event):
         if self.barraExpandida:
             self.menuComada = tk.Menu(self.ventana, tearoff=0)
@@ -96,6 +100,17 @@ class GestionRegistrador():
        
     def informeDiario(self):
         InformeDiario(self.usuario)
+
+    #Salir
+    def crearMenuSalir(self, event):
+        if self.barraExpandida:
+            self.menuSalir = tk.Menu(self.ventana, tearoff=0)
+            self.menuSalir.add_command(label="Salir", command=self.salir)
+            self.menuSalir.add_separator()
+            self.menuSalir.post(self.barra.winfo_rootx() + 80, self.barra.winfo_rooty() + 198)
+    
+    def salir(self):
+        self.ventana.destroy()
 
     def __init__(self, loggin, usuario):
         self.ventana = tk.Toplevel(loggin)
@@ -111,6 +126,7 @@ class GestionRegistrador():
 
         # Variable para saber si la barra está expandida
         self.barraExpandida = False
+        self.bandera = True
 
         # Iconos
         self.iconoBarra = tk.PhotoImage(file=r"Restaurante/Src/barra.png")
@@ -126,29 +142,35 @@ class GestionRegistrador():
         self.btnBarra.bind("<Button-1>", self.toggleBarra)
         Tooltip(self.btnBarra, "Expandir o contraer el menú")
 
-        self.btnGestionChef = tk.Label(self.barra, text="Gestionar Chef")
+        self.btnGestionChef = tk.Label(self.barra, text="Gestionar Chef", bg="#CCD1D1")
         self.btnGestionChef.place(relx=0.01, rely=0.1, anchor="w")
         self.btnGestionChef.place_forget()
         self.btnGestionChef.bind("<Button-1>", self.crearMenuChef)
         Tooltip(self.btnGestionChef, "Registrar o eliminar un chef")
 
-        self.btnGestionMesero = tk.Label(self.barra, text="Gestionar Mesero")
+        self.btnGestionMesero = tk.Label(self.barra, text="Gestionar Mesero", bg="#CCD1D1")
         self.btnGestionMesero.place(relx=0.01, rely=0.3, anchor="w")
         self.btnGestionMesero.place_forget()
         self.btnGestionMesero.bind("<Button-1>", self.crearMenuMesero)
         Tooltip(self.btnGestionMesero, "Registrar o eliminar un mesero")
 
-        self.btnGestionMesa = tk.Label(self.barra, text="Gestionar Mesa")
+        self.btnGestionMesa = tk.Label(self.barra, text="Gestionar Mesa", bg="#CCD1D1")
         self.btnGestionMesa.place(relx=0.01, rely=0.4, anchor="w")
         self.btnGestionMesa.place_forget()
         self.btnGestionMesa.bind("<Button-1>", self.crearMenuMesa)
         Tooltip(self.btnGestionMesa, "Registrar o eliminar una mesa")
 
-        self.btnGestionComanda = tk.Label(self.barra, text="Gestionar Comandas")
+        self.btnGestionComanda = tk.Label(self.barra, text="Gestionar Comandas", bg="#CCD1D1")
         self.btnGestionComanda.place(relx=0.01, rely=0.5, anchor="w")
         self.btnGestionComanda.place_forget()
         self.btnGestionComanda.bind("<Button-1>", self.crearMenuComada)
         Tooltip(self.btnGestionComanda, "Gestionar comandas y generar informes")
+
+        self.btnSalir = tk.Label(self.barra, text="Salir", bg="#CCD1D1")
+        self.btnSalir.place(relx=0.01, rely=0.6, anchor="w")
+        self.btnSalir.place_forget()
+        self.btnSalir.bind("<Button-1>", self.crearMenuSalir)
+        Tooltip(self.btnSalir, "Presione para salir de la ventana")
 
         self.btnAyuda = tk.Label(self.ventana, image=self.iconoAyuda)
         self.btnAyuda.place(relx=0.95, rely=0.05, anchor="center")
